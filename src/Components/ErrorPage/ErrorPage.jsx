ac37c0f5da92725e41b80de601375635f47bcd82
MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2024-02-11 20:38:08 +0600

    error page fixed

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-red-600 text-center my-20">
+    <div className="flex justify-center items-center errorPage">
+      <h1 className="inline-block text-4xl font-bold text-red-600 text-center align-middle">
         ⚠️ 404 not found. Page does not exist❗️❗️
       </h1>
     </div>
